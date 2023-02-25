2c477cb8f259dc78019f85450aff96c52499a604
Attornado <73835871+attornado@users.noreply.github.com>
2023-02-25 02:28:58 +0100

    Improved some documentation.
    Signed-off-by: Attornado <73835871+Attornado@users.noreply.github.com>

--- models/pretraining/graph_infomax.py
+++ models/pretraining/graph_infomax.py
@@ -449,17 +449,17 @@ class DeepGraphInfomaxV2(DeepGraphInfomax, SerializableModule):
 
 def train_step_DGI(model: DeepGraphInfomaxV2, train_data: DataLoader, optimizer, device,
                    use_edge_weight: bool = False, use_edge_attr: bool = False):
-    # put the model in training mode
+    # Put the model in training mode
     model.train()
 
-    # running average loss over the batches
+    # Running average loss over the batches
     running_loss = 0.0
     steps: int = 1
 
     for data in iter(train_data):
-        # move batch to device
+        # Move batch to device
         data = data.to(device)
-        # reset the optimizer gradients
+        # Reset the optimizer gradients
         optimizer.zero_grad()
 
         # Encoder output
